89a0f042cfd501681b4baa8e1a301326885ebc42
Laura Lindzey <lindzey@uw.edu>
2022-12-05 09:38:26 -0800

    Add scripts to generate list of ICECAP files at NSIDC and download them

---
+++ code/download/download_utig_nsidc.py
@@ -0,0 +1,257 @@
+#! /usr/bin/env python3
+"""
+Download UTIG's files from NSIDC, as specified in the
+input CSV.
+
+Since every UTIG datacenter has a different directory organization,
+I've decided to organize UTIG radargrams as:
+[region]/UTIG/[campaign]/[PST]/[filename].nc
+
+This script creates that directory structure and downloads radargrams
+into the correct place.
+---------------------------------------------------------
+Modified from one of NSIDC's generated download scripts:
+---------------------------------------------------------
+# NSIDC Data Download Script
+#
+# Copyright (c) 2022 Regents of the University of Colorado
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+"""
+
+import pathlib
+import os
+import csv
+import base64
+import math
+import netrc
+import os.path
+import sys
+import time
+from getpass import getpass
+
+from urllib.parse import urlparse
+from urllib.request import urlopen, Request, build_opener, HTTPCookieProcessor
+from urllib.error import HTTPError, URLError
+
+
+def get_username():
+    username = ''
+    do_input = input
+    username = do_input(
+        'Earthdata username (or press Return to use a bearer token): ')
+    return username
+
+
+def get_password():
+    password = ''
+    while not password:
+        password = getpass('password: ')
+    return password
+
+
+def get_token():
+    token = ''
+    while not token:
+        token = getpass('bearer token: ')
+    return token
+
+
+def get_login_credentials():
+    """Get user credentials from .netrc or prompt for input."""
+    credentials = None
+    token = None
+    URS_URL = 'https://urs.earthdata.nasa.gov'
+
+    try:
+        info = netrc.netrc()
+        username, _, password = info.authenticators(
+            urlparse(URS_URL).hostname)
+        if username == 'token':
+            token = password
+        else:
+            credentials = '{0}:{1}'.format(username, password)
+            credentials = base64.b64encode(
+                credentials.encode('ascii')).decode('ascii')
+    except Exception:
+        username = None
+        password = None
+
+    if not username:
+        username = get_username()
+        if len(username):
+            password = get_password()
+            credentials = '{0}:{1}'.format(username, password)
+            credentials = base64.b64encode(
+                credentials.encode('ascii')).decode('ascii')
+        else:
+            token = get_token()
+
+    return credentials, token
+
+
+def get_speed(time_elapsed, chunk_size):
+    if time_elapsed <= 0:
+        return ''
+    speed = chunk_size / time_elapsed
+    if speed <= 0:
+        speed = 1
+    size_name = ('', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    i = int(math.floor(math.log(speed, 1000)))
+    p = math.pow(1000, i)
+    return '{0:.1f}{1}B/s'.format(speed / p, size_name[i])
+
+
+def output_progress(count, total, status='', bar_len=60):
+    if total <= 0:
+        return
+    fraction = min(max(count / float(total), 0), 1)
+    filled_len = int(round(bar_len * fraction))
+    percents = int(round(100.0 * fraction))
+    bar = '=' * filled_len + ' ' * (bar_len - filled_len)
+    fmt = '  [{0}] {1:3d}%  {2}   '.format(bar, percents, status)
+    print('\b' * (len(fmt) + 4), end='')  # clears the line
+    sys.stdout.write(fmt)
+    sys.stdout.flush()
+
+
+def cmr_read_in_chunks(file_object, chunk_size=1024 * 1024):
+    """Read a file in chunks using a generator. Default chunk size: 1Mb."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
+def get_login_response(url, credentials, token):
+    opener = build_opener(HTTPCookieProcessor())
+
+    req = Request(url)
+    if token:
+        req.add_header('Authorization', 'Bearer {0}'.format(token))
+    elif credentials:
+        try:
+            response = opener.open(req)
+            # We have a redirect URL - try again with authorization.
+            url = response.url
+        except HTTPError:
+            # No redirect - just try again with authorization.
+            pass
+        except Exception as e:
+            print('Error{0}: {1}'.format(type(e), str(e)))
+            sys.exit(1)
+
+        req = Request(url)
+        req.add_header('Authorization', 'Basic {0}'.format(credentials))
+
+    try:
+        response = opener.open(req)
+    except HTTPError as e:
+        err = 'HTTP error {0}, {1}'.format(e.code, e.reason)
+        if 'Unauthorized' in e.reason:
+            if token:
+                err += ': Check your bearer token'
+            else:
+                err += ': Check your username and password'
+        print(err)
+        sys.exit(1)
+    except Exception as e:
+        print('Error{0}: {1}'.format(type(e), str(e)))
+        sys.exit(1)
+
+    return response
+
+
+def nsidc_download(dest_dir, url):
+    """
+    Download file at URL into dest_dir.
+    """
+    print("Downloading {} to {}".format(url, dest_dir))
+    force = False  # Force re-download
+    quiet = False  # Suppress debug messages
+    credentials = None
+    token = None
+
+    if not credentials and not token:
+        p = urlparse(url)
+        if p.scheme == 'https':
+            credentials, token = get_login_credentials()
+
+    filename = url.split('/')[-1]
+    dest_filepath = os.path.join(dest_dir, filename)
+
+    try:
+        response = get_login_response(url, credentials, token)
+        length = int(response.headers['content-length'])
+        try:
+            if not force and length == os.path.getsize(dest_filepath):
+                if not quiet:
+                    print('  File exists, skipping')
+                    return
+        except OSError:
+            pass
+        count = 0
+        chunk_size = min(max(length, 1), 1024 * 1024)
+        max_chunks = int(math.ceil(length / chunk_size))
+        time_initial = time.time()
+        with open(dest_filepath, 'wb') as out_file:
+            for data in cmr_read_in_chunks(response, chunk_size=chunk_size):
+                out_file.write(data)
+                if not quiet:
+                    count = count + 1
+                    time_elapsed = time.time() - time_initial
+                    download_speed = get_speed(
+                        time_elapsed, count * chunk_size)
+                    output_progress(count, max_chunks,
+                                    status=download_speed)
+        if not quiet:
+            print()
+    except HTTPError as e:
+        print('HTTP error {0}, {1}'.format(e.code, e.reason))
+    except URLError as e:
+        print('URL error: {0}'.format(e.reason))
+    except IOError:
+        raise
+
+
+def main(url_filepath, data_dir):
+    print("Loading metadata from {}".format(url_filepath))
+    with open(url_filepath) as fp:
+        csv_reader = csv.DictReader(fp)
+        # TODO: These should wind up in the csv file as well.
+        region = "ANTARCTIC"
+        campaign = "ICECAP"
+        for row in csv_reader:
+            # fields are institution,flight,segment,granule,url
+            print(row)
+            institution = row['institution']
+            pst = row['segment']
+            pst_dir = os.path.join(
+                data_dir, region, institution, campaign, pst)
+            if not os.path.isdir(pst_dir):
+                try:
+                    pp = pathlib.Path(pst_dir)
+                    pp.mkdir(parents=True, exist_ok=True)
+                except Exception as ex:
+                    print("Could not create {}".format(pst_dir))
+                    raise (ex)
+            url = row['url']
+            nsidc_download(pst_dir, url)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "url_list", help="CSV file specifying URLs for all radargrams")
+    parser.add_argument("data_directory",
+                        help="Root directory for all QIceRadar-managed radargrams.")
+    args = parser.parse_args()
+    main(args.url_list, args.data_directory)
---
+++ code/download/generate_utig_nsidc_index.py
@@ -0,0 +1,145 @@
+#! /usr/bin/env python3
+"""
+In the future, I want data providers to be able to add one-off profiles
+by simply editing a CSV file with the appropriate fields.
+
+So, to experiment with that workflow, I'm dividign the UTIG download
+into creating the index and then downloading it.
+
+At NSIDC, UTIG's data is at:
+* hicars1: https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IR1HI1B.001/
+* hicars2: https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IR2HI1B.001/
+
+The directory structure is
+* IR1HI1B.001 / IR2HI1B.001
+  * yyyy.mm.dd/
+    * {instrument}_{yyyy}{doy}_{project}_{set}_{transect}_{granule}.nc
+
+TODO: See if the authentication used in download_utig_nsidc is faster
+in this script. As is, it is shockingly slow.
+"""
+
+from bs4 import BeautifulSoup
+import csv
+import netrc  # Used to parse authentication token from ~/.netrc
+import os
+import pathlib
+import re
+import requests
+
+
+def credentials_from_netrc():
+    hostname = 'urs.earthdata.nasa.gov'
+    try:
+        nn = netrc.netrc()
+        username, _, token = nn.authenticators(hostname)
+        if username != "token":
+            msg = "This function only supports logging in via authentication tokens."
+            print(msg)
+            raise Exception(msg)
+    except FileNotFoundError as ex:
+        print("Can't authenticate -- .netrc file not found")
+        raise (ex)
+
+    return token
+
+
+def main(index_filepath):
+    print("Saving index to: {}".format(index_filepath))
+    # Crawling the NSIDC website finding URLs is terribly slow, so we
+    # need to be able to resume.
+    previous_csv = None
+    # By checking previous URLs, we'll capture both data product and flight/day
+    previous_urls = set()
+    if os.path.isfile(index_filepath):
+        print("loading index!")
+
+        with open(index_filepath) as fp:
+            csv_reader = csv.DictReader(fp)
+            previous_csv = []
+            for dd in csv_reader:
+                institution = dd['institution']
+                flight = dd['flight']
+                url = dd['url']
+                date_url = '/'.join(url.split('/')[:-1])
+                previous_urls.add(date_url)
+                previous_csv.append("{},{},{},{},{}\n".format(
+                    institution, flight, dd['segment'], dd['granule'], url))
+        previous_urls = list(previous_urls)
+        previous_urls.sort()
+        print(previous_urls)
+
+    hicars1_url = "https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IR1HI1B.001"
+    hicars2_url = "https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IR2HI1B.001"
+    # Extract information from the filename
+    regex = "(?P<instrument>[0-9a-zA-Z]+)_(?P<year>[0-9]{4})(?P<doy>[0-9]{3})_(?P<project>[0-9a-zA-Z]+)_(?P<set>[0-9a-zA-Z]+)_(?P<transect>[0-9a-zA-Z]*)_(?P<granule>[0-9]*).nc"
+    token = credentials_from_netrc()
+    with requests.sessions.Session() as session, open(index_filepath, 'w') as fp:
+        fp.write("institution,flight,segment,granule,url\n")
+        if previous_csv is not None:
+            fp.writelines(previous_csv)
+
+        session.headers.update({'Authorization': 'Bearer {0}'.format(token)})
+        for instrument_url in [hicars1_url, hicars2_url]:
+            print("*************")
+            print("Checking {}".format(instrument_url))
+            instrument_resp = session.get(instrument_url)
+            instrument_soup = BeautifulSoup(
+                instrument_resp.text, 'html.parser')
+
+            # Each link shows up a few times
+            flight_days = set([link.get('href').strip('/') for link in instrument_soup.find_all('a')
+                               if re.match("[0-9]{4}.[0-9]{2}.[0-9]{2}", link.get('href')) is not None])
+            flight_days = list(flight_days)
+            flight_days.sort()
+            print(flight_days)
+            for flight_day in flight_days:
+                print("Listing segments for {}".format(flight_day))
+                flight_url = "{}/{}".format(instrument_url, flight_day)
+                print(flight_url)
+                if flight_url in previous_urls:
+                    print("Skipping {} -- already scraped".format(flight_day))
+                    continue
+                try:
+                    flight_resp = session.get(flight_url)
+                except requests.exceptions.ConnectionError as ex:
+                    print(ex)
+                    print("...trying again")
+                    try:
+                        flight_resp = session.get(flight_url)
+                    except Exception as ex:
+                        err_msg = "WARNING: failed to index {}; re-run script".format(
+                            flight_url)
+                        print(err_msg)
+                        # Go ahead and continue because I won't be constantly monitoring the script.
+                        continue
+
+                flight_soup = BeautifulSoup(flight_resp.text, 'html.parser')
+                segment_files = set([link.get('href') for link in flight_soup.find_all('a')
+                                     if link.get('href').endswith('nc')])
+                segment_files = list(segment_files)
+                segment_files.sort()
+                print(segment_files)
+                for segment_file in segment_files:
+                    mm = re.match(regex, segment_file)
+                    _, _, _, project, ss, transect, granule = mm.groups()
+                    pst = "_".join((project, ss, transect))
+
+                    segment_url = "{}/{}".format(flight_url, segment_file)
+                    fp.write("{},{},{},{},{}\n".format(
+                        "UTIG", flight_day, pst, granule, segment_url))
+
+
+if __name__ == "__main__":
+    # TODO: better way to figure this out =)
+    package_dir = "/Users/lindzey/Documents/QIceRadar/radar_wrangler"
+    index_dir = os.path.join(package_dir, "data", "UTIG")
+    try:
+        pp = pathlib.Path(index_dir)
+        pp.mkdir(parents=True, exist_ok=True)
+    except Exception as ex:
+        print("Could not create {}".format(index_dir))
+        print(ex)
+        raise (ex)
+    index_filepath = os.path.join(index_dir, "utig_nsidc_index.csv")
+    main(index_filepath)
